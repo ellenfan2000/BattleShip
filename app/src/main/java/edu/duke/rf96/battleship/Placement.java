b423cfe1e94f5942feb4dd89ed5e5a30d82ad45e
Ellen Fan <ellen.fan.rui@gmail.com>
2023-01-25 14:51:09 -0500

    goal 1 task 4

---
+++ app/src/main/java/edu/duke/rf96/battleship/Placement.java
@@ -0,0 +1,70 @@
+package edu.duke.rf96.battleship;
+
+/*
+This class describe the placement of a ship
+including coordinate and oritation
+orientation V: vertical, H: horizontal
+where: coordinate of the top left corner of the ship
+ */
+public class Placement {
+  // where: coordinate of the top left corner of the ship
+  private final Coordinate where;
+
+  // orientation V: vertical, H: horizontal
+  private final char orien;
+
+  /*
+   * Construct a Placement
+   */
+  public Placement(Coordinate w, char or) {
+    where = w;
+    orien = or;
+  }
+
+  /*
+   * Construct a Placement
+   * 
+   * @param descr : string got from players in the form "A3H", "B9H"
+   */
+  public Placement(String descr) {
+    if (descr.length() != 3) {
+      throw new IllegalArgumentException("Placement should have 3 characters but have " + descr.length());
+    }
+    descr = descr.toUpperCase();
+    String coorStr = descr.substring(0, 2);
+    char orienC = descr.charAt(2);
+
+    where = new Coordinate(coorStr);
+    if (orienC != 'H' && orienC != 'V') {
+      throw new IllegalArgumentException("Placement Orientation should be H or V but is " + orienC);
+    }
+    orien = orienC;
+  }
+
+  public Coordinate getCoordinate() {
+    return where;
+  }
+
+  public char getOrientation() {
+    return orien;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o.getClass().equals(getClass())) {
+      Placement p = (Placement) o;
+      return where.equals(p.getCoordinate()) && orien == p.getOrientation();
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return where.toString() + ": " + orien;
+  }
+
+  @Override
+  public int hashCode() {
+    return toString().hashCode();
+  }
+}
---
+++ app/src/test/java/edu/duke/rf96/battleship/PlacementTest.java
@@ -0,0 +1,88 @@
+package edu.duke.rf96.battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PlacementTest {
+  @Test
+  public void test_Initialize() {
+    Coordinate c1 = new Coordinate(1, 2);
+    Placement p1 = new Placement(c1, 'H');
+    assertEquals(p1.getCoordinate(), c1);
+    assertEquals(p1.getOrientation(), 'H');
+  }
+
+  @Test
+  public void test_string_construct_valid() {
+    Placement p1 = new Placement("A3H");
+    assertEquals(0, p1.getCoordinate().getRow());
+    assertEquals(3, p1.getCoordinate().getColumn());
+    assertEquals('H', p1.getOrientation());
+
+    Placement p2 = new Placement("G9v");
+    assertEquals(6, p2.getCoordinate().getRow());
+    assertEquals(9, p2.getCoordinate().getColumn());
+    assertEquals('V', p2.getOrientation());
+
+    Placement p3 = new Placement("A3h");
+    assertEquals(0, p3.getCoordinate().getRow());
+    assertEquals(3, p3.getCoordinate().getColumn());
+    assertEquals('H', p1.getOrientation());
+
+    Placement p4 = new Placement("a3H");
+    assertEquals(0, p4.getCoordinate().getRow());
+    assertEquals(3, p4.getCoordinate().getColumn());
+    assertEquals('H', p1.getOrientation());
+  }
+
+  @Test
+  public void test_string_construct_error() {
+    assertThrows(IllegalArgumentException.class, () -> new Placement("123"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("A2D"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("12."));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("C103"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("A"));
+    assertThrows(IllegalArgumentException.class, () -> new Placement("A5d"));
+  }
+
+  @Test
+  public void test_equals() {
+    Placement p1 = new Placement("A3H");
+    Placement p2 = new Placement("a3h");
+    Placement p3 = new Placement("a3V");
+    Placement p4 = new Placement("B3V");
+
+    assertEquals(p1, p2);
+    assertEquals(p2, p1);
+    assertNotEquals(p1, p3);
+    assertNotEquals(p3, p4);
+    assertNotEquals(p1, "(0, 3): H");
+  }
+
+  @Test
+  public void test_toString() {
+    Placement p1 = new Placement("A3H");
+    Placement p2 = new Placement("B3h");
+    Placement p3 = new Placement("D9V");
+
+    assertEquals(p1.toString(), "(0, 3): H");
+    assertEquals(p2.toString(), "(1, 3): H");
+    assertEquals(p3.toString(), "(3, 9): V");
+
+  }
+
+  @Test
+  public void test_hash() {
+    Placement p1 = new Placement("A3H");
+    Placement p2 = new Placement("a3h");
+    Placement p3 = new Placement("a3V");
+    Placement p4 = new Placement("B3V");
+    assertEquals(p1.hashCode(), p2.hashCode());
+    assertNotEquals(p1.hashCode(), p3.hashCode());
+    assertNotEquals(p3.hashCode(), p4.hashCode());
+    assertNotEquals(p2.hashCode(), p4.hashCode());
+
+  }
+
+}
